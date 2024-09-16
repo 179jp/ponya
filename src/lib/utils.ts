b28d290f03707e4f66431e1150dbb573fb1017ee
nnjyami <y@179.jp>
2024-09-16 13:43:20 +0900

    一覧ページの調整

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,11 @@
 type DateStyle = Intl.DateTimeFormatOptions['dateStyle'];
 
-export function formatDate(date: string, dateStyle: DateStyle = 'medium', locales = 'en') {
+export function formatDate(date: string, dateStyle: DateStyle = 'medium', isOnlyDay = false, locales = 'en') {
 	// Safari is mad about dashes in the date
 	const dateToFormat = new Date(date.replaceAll('-', '/'));
-	const dateFormatter = new Intl.DateTimeFormat(locales, { dateStyle });
+	const options: Intl.DateTimeFormatOptions = isOnlyDay
+		? { day: 'numeric' }
+		: { year: 'numeric', month: 'numeric', day: 'numeric' };
+	const dateFormatter = new Intl.DateTimeFormat(locales, options);
 	return dateFormatter.format(dateToFormat);
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,19 @@
 <!-- Posts -->
 <section class="f1">
 	<div class="featureHeader">
-		<h1>過剰にモノを並べて表現する</h1>
+		<h1>過剰に要素を並べて表現する</h1>
 		<p class="neueHaas-thin">Express things by arranging them<br />excessively on the web.</p>
 	</div>
 	<ul class="posts">
 		{#each data.posts as post}
 			<li class="post">
-				<a href={post.slug} class="title">{post.title}</a>
-				<p class="date">{formatDate(post.date)}</p>
-				<p class="description">{post.description}</p>
+				<a href={post.slug} class="post_wrap">
+					<span class="post_day neueHaas-mid">{formatDate(post.date, 'medium', true)}</span>
+					<p class="post_info">
+						<span class="post_title">{post.title}</span>
+						<span class="post_english neueHaas-thin">{post.titleEnglish}</span>
+					</p>
+				</a>
 			</li>
 		{/each}
 	</ul>
@@ -50,5 +54,35 @@
 	}
 
 	.posts {
+	}
+
+	.post {
+		margin-top: 1em;
+	}
+	.post_wrap {
+		color: #333;
+		display: grid;
+		gap: 0.5em;
+		grid-template-columns: 2em 1fr;
+		width: 100%;
+		overflow: hidden;
+	}
+	.post_info {
+		display: flex;
+		flex-direction: column;
+		padding-top: 1.5em;
+		position: relative;
+	}
+	.post_title {
+		font-size: clamp(1.25rem, calc(1.25rem + 1vw), 1.8rem);
+		display: inline-block;
+		height: 400px;
+		position: absolute;
+		top: 0;
+		left: 0;
+		rotate: -90deg;
+		transform-origin: right top;
+		translate: -1.6em -0.4em;
+		writing-mode: vertical-rl;
 	}
 </style>
