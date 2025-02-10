bff4101de9a5a11b9fe52b4c497df8ee934ab681
nnjyami <y@179.jp>
2025-02-10 14:26:10 +0900

    ライブラリ変更

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment';
-
-export const title = 'ぽんや';
-export const description = 'SvelteKit blog for poets';
-export const url = dev ? 'http://localhost:5173/' : 'https://ponya.xyz/';
--- src/routes/poncho/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PonchoPost } from '$lib/types';
-
-export async function load({ fetch }) {
-	const response = await fetch('api/posts/poncho');
-	const posts: PonchoPost[] = await response.json();
-	return { posts };
-}
